50cf76222eb96d5450659e228606687d8d2cab92
jeacqueline.rios-srwity <jeacqueline.rios@outlook.com>
2023-01-20 01:23:00 -0600

    correct some errors

--- js/script.js
+++ js/script.js
@@ -1,29 +1,40 @@
-//To show current date
 let actualDate = document.getElementById("currentDay");
 actualDate.innerHTML = dayjs().format("dddd, MMMM D.");
-
 const apiKey = "1facf4f0c87aa3b6bbc4c5a2f188d2bc";
-let btnCity = document.querySelector("#btnCity");
+let btnCity = document.querySelector("#btnCitySearch");
 let userSearch = document.querySelector("#userSearch");
-let lastSearch = document.querySelector("#lastSearch"); 
+let lastSearch = document.querySelector("#recentsSearch"); 
 
+// Add event listener to the button
+btnCity.addEventListener('click', getData);
 
-
-function getData () {
-    navigator.geolocation.getCurrentPosition((position) => {
-        let (lat, long) = position.coords;
-
-        fetch (`api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`)
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-            showData(data);
-        })
-
+// Get the city name from the input
+function getData() {
+  let city = userSearch.value;
+  // Send a fetch request with the city name
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      showData(data);
+      // Save the data to localStorage
+      localStorage.setItem("weather", JSON.stringify(data));
     })
+    .catch(error => {
+      console.log(error);
+    });
 }
 
-function showData (data) {
-    let {} = data.current; 
-
+// Display the data on the page
+function showData(data) {
+  let displayCity = document.querySelector("#displayCity");
+  let temp = document.querySelector("#temp");
+  let wind = document.querySelector("#wind");
+  let humidity = document.querySelector("#humidity");
+  if (data.main) {
+    displayCity.textContent = data.name;
+    temp.textContent = data.main.temp;
+    wind.textContent = data.wind.speed;
+    humidity.textContent = data.main.humidity;
+  }
 }
